abb62f104f03ad1417948e2f3834c6385f53a2e0
unknown <arbhuiyan.pits@gmail.com>
2021-11-19 19:24:25 +0600

    first commit

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
       <footer class="sticky-footer">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright © Saif ERP <?php echo date('Y') ?></span>
+            <span>Copyright © Smart Housing Ltd <?php echo date('Y') ?></span>
           </div>
         </div>
       </footer>
