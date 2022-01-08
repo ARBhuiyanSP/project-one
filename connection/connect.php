dc8e41d55ca9defaa9e7a86e26e5e5d2f12e5961
ARBhuiyanSP <arbhuiyan.pits@gmail.com>
2022-01-08 11:06:54 +0600

    update

--- connection/connect.php
+++ connection/connect.php
@@ -5,8 +5,8 @@
 global $conn;
 $servername = "localhost";
 $username   = "root";
-$password   = "ms@034571";
-$dbname     = "inventory_module";
+$password   = "Ms@034571";
+$dbname     = "project_one";
 
 // Create connection
 $conn       = new mysqli($servername, $username, $password, $dbname);
